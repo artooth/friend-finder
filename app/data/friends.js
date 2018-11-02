929f825db0768579dc8bbd0a8898f81763f52502
artooth <artooth@gmail.com>
2018-11-02 17:53:30 -0500

    nov 2

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+        "name": 'Justin Timberlake',
+        "photo": 'https://s3.r29static.com//bin/entry/4c5/x,80/1705671/image.jpg',
+        "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        "name": "Chris Pratt",
+        "photo": "https://media.glamour.com/photos/5a2170095b0bbf05717ba1b9/master/w_644,c_limit/chris-pratt-fake-facebook-profile.jpg",
+        "scores": [5, 5, 1, 1, 3, 2, 1, 1, 2, 1]
+    },
+    {
+        "name": "Robert Downey jr",
+        "photo": "https://periodicocorreo.com.mx/wp-content/uploads/2015/12/ocio-bn-robert-downey.jpg",
+        "scores": [1, 4, 2, 2, 1, 2, 5, 2, 3, 4]
+    },
+    {
+        "name": "Conor Mcgregor",
+        "photo": "https://www.ecestaticos.com/imagestatic/clipping/e27/d07/e27d07e8e7a03e80b1202c04fc77f5e0/imagen-sin-titulo.jpg?mtime=1516271646",
+        "scores": [3, 2, 1, 4, 5, 2, 2, 2, 1, 3]
+    },
+    {
+        "name": "Henry Golding",
+        "photo": "https://3.bp.blogspot.com/-IluPNY26iG8/Wt7h_dq0mfI/AAAAAAAACH0/dgEcyakC0m4SkgfwGCdpUDCNChPE1bb0ACLcBGAs/s1600/henry%2Bgolding.jpg",
+        "scores": [5, 5, 3, 4, 1, 4, 2, 1, 2, 5]
+    },
+    {
+        "name": "Caitlyn Jenner",
+        "photo": "https://24.p3k.hu/app/uploads/2015/10/caitlyn-jenner-e1445527409276.jpg",
+        "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        "name": "Kim Kardashian",
+        "photo": "https://www.biography.com/.image/t_share/MTU0MTU0MTcyNDI0Mzk3OTg0/kim-kardashian-signs-copies-of-her-new-book-photo-by-michael-loccisanogetty-imagesjpg-square.jpg",
+        "scores": [3, 2, 1, 1, 2, 3, 5, 2, 1, 2]
+    },
+    {
+        "name": "Jennifer Lawrence",
+        "photo": "https://www.thewrap.com/wp-content/uploads/2016/09/jlawmockingjay5.jpg",
+        "scores": [5, 5, 3, 1, 3, 2, 3, 2, 1, 5]
+    },
+    {
+        "name": "Amy Adams",
+        "photo": "https://www.irishtimes.com/polopoly_fs/1.3558801.1531126515!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+        "scores": [4, 3, 5, 2, 1, 4, 2, 2, 1, 3]
+    },
+    {
+        "name": "Simone Biles",
+        "photo": "https://mediaassets.wcpo.com/photo/2017/09/25/WCPO_Simone_Biles_1506358856547_67142725_ver1.0_640_480.jpg",
+        "scores": [4, 5, 3, 1, 4, 2, 1, 3, 5, 2]
+    },
+
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -7,6 +7,14 @@ let app = express();
 let PORT = process.env.PORT || 8080;
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+require(".app/routing/apiRoutes.js")(app);
+require(".app/routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT, function () {
